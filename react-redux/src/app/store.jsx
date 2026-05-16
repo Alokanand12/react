033672514749f9b9cca9525beb81dd512a29ee9e
Alok Anand <sandilyaalokanand@gmail.com>
2026-05-16 18:57:28 +0530

    upload project

--- react-redux/src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counterReducer";
-
-export let store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
